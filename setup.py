9b5aac1afb66fdfac9005498f881c0739034ca78
Jon Gunderson <jongund@illinois.edu>
2017-03-06 16:59:26 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ classifiers = [
     'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 
-setup(name="pyia",
-      description="Python MSAA client library",
+setup(name="pyia2",
+      description="Python MSAA+IAccessible2 client library",
       long_description = __doc__,
-      author="Eitan Isaacson",
-      author_email="eitan@ascender.com",
-      url="http://github.com/eeejay/pyia/tree/master",
-      download_url = "http://github.com/eeejay/pyia/tree/master",
+      author="Jon Guderson",
+      author_email="jongund@illinois.edu",
+      url="http://github.com/jongund/pyia2/tree/master",
+      download_url = "http://github.com/jongund/pyia2/tree/master",
       license="LGPLv2.2",
       classifiers=classifiers,
       version=pyia.__version__,
-      packages=["pyia"])
+      packages=["pyia2"])
